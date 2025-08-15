4c6aadde0cb0e9279a20d08c0704d7a3cda485ac
kko182 <kko182@gmail.com>
2025-08-15 10:29:00 -0500

    added list of future updates

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+# Add a scoring system
+# Implement multiple lives and respawning
+# Add an explosion effect for the asteroids
+# Add acceleration to the player movement
+# Make the objects wrap around the screen instead of disappearing
+# Add a background image
+# Create different weapon types
+# Make the asteroids lumpy instead of perfectly round
+# Make the ship have a triangular hit box instead of a circular one
+# Add a shield power-up
+# Add a speed power-up
+# Add bombs that can be dropped
 import sys
 import pygame
 from constants import *
